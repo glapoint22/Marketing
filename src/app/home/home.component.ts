37fb955e1ecf33b884b1a698edb18c8a44210c0b
glapoint22 <glapoint22@gmail.com>
2017-10-10 21:40:19 -0400

    Started the banner images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,17 @@ import { DataService } from "../data.service";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public categories: Array<object> = [{"name":"All Categories","value":"All"},{"name":"Books","value":"Books"}];
+  public selectedCategory: Object = {};
+  public bannerImages: Array<object> = [{"name":"Costumes.png","url":"http://www.walmar.com"},{"name":"Halloween.png","url":"http://www.target.com"},{"name":"2WeekDiet.png","url":"http://www.amazon.com"}];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.selectedCategory = this.categories[0];
+  }
+
+  onBannerArrowClick(){
+    console.log("Hello");
   }
 }
