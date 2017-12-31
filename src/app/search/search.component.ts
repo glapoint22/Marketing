f2e07d76d4c8edbaae46164dec82cebec8d2ce1c
glapoint22 <glapoint22@gmail.com>
2017-12-31 15:51:27 -0500

    Completed search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from "../data.service";
+import { FilterService } from "./../filter.service";
 
 @Component({
   selector: 'app-search',
@@ -19,60 +20,77 @@ export class SearchComponent implements OnInit {
   private categories;
   private filters;
 
+  //Sort Options
   private selectedSortOption: any;
   private sortOptions = [
     {
-      name: 'Relevance',
+      name: 'Sort by Relevance',
       value: 'relevance'
     },
     {
-      name: 'Price: Low to High',
+      name: 'Sort by Price: Low to High',
       value: 'price-asc'
     },
     {
-      name: 'Price: High to Low',
+      name: 'Sort by Price: High to Low',
       value: 'price-desc'
     }
   ];
 
-  
+  //Products per page
+  private productsPerPage: number;
+  private perPageOptions = [24, 48, 72, 96];
 
-  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router, private filterService: FilterService) { }
 
   ngOnInit() {
-    
-
     this.route.queryParamMap.subscribe(queryParams => {
       let parameters: Array<any> = [];
-
       this.query = queryParams.get('query');
 
-      setTimeout(()=> {
-        let index = this.sortOptions.findIndex(x => x.value == queryParams.get('sort'));
-        this.selectedSortOption =  this.sortOptions[index == -1 ? 0 : index]; 
+      //Populate the dropdowns
+      setTimeout(() => {
+        let index = this.sortOptions.findIndex(x => x.value === queryParams.get('sort'));
+        this.selectedSortOption = this.sortOptions[index == -1 ? 0 : index];
+
+        let perPage = Number(queryParams.get('limit'));
+        this.productsPerPage = perPage === 0 ? this.perPageOptions[0] : perPage;
       });
 
-      
-      
+      //Set the parameters array from the query params
       for (let i = 0; i < queryParams.keys.length; i++) {
-        parameters.push({ key: queryParams.keys[i], value: queryParams.get(queryParams.keys[i]) })
+        parameters.push({ key: queryParams.keys[i], value: queryParams.get(queryParams.keys[i]) });
       }
 
+      //Set the default sort option if not part of the query params
+      if (parameters.findIndex(x => x.key === 'sort') === -1) {
+        parameters.push({ key: 'sort', value: this.sortOptions[0].value });
+      }
+
+      //Set the default per page option if not part of the query params
+      if (parameters.findIndex(x => x.key === 'limit') === -1) {
+        parameters.push({ key: 'limit', value: this.perPageOptions[0] });
+      }
+
+      //Get the products
       this.dataService.get('api/Products', parameters)
         .subscribe((response: any) => {
-          let resultsPerPage = response.resultsPerPage, body = document.scrollingElement || document.documentElement;
-          body.scrollTop = 0;
+          //Assign properties and variables
+          let body = document.scrollingElement || document.documentElement;
           this.pageList = [];
           this.page = response.page;
           this.products = response.products;
           this.totalProducts = response.totalProducts;
-          this.productStart = resultsPerPage * (this.page - 1) + 1;
+          this.productStart = this.productsPerPage * (this.page - 1) + 1;
           this.productEnd = this.productStart + response.products.length - 1;
-          this.pages = Math.ceil(this.totalProducts / resultsPerPage);
+          this.pages = Math.ceil(this.totalProducts / this.productsPerPage);
           this.categories = response.categories;
           this.filters = response.filters;
 
+          //Scroll to top
+          body.scrollTop = 0;
 
+          //Pages
           this.pageList.push('1');
           if (this.page >= 5 && this.pages > 7) {
             this.pageList.push('...');
@@ -86,13 +104,7 @@ export class SearchComponent implements OnInit {
                 this.pageList.push(i.toString());
               }
             }
-
-
             if (this.pages - this.page > 3) this.pageList.push('...');
-
-
-
-
           } else {
             for (let i = 2; i <= Math.min(this.pages - 1, 6); i++) {
               this.pageList.push(i.toString());
@@ -100,10 +112,6 @@ export class SearchComponent implements OnInit {
             if (this.pages > 7) this.pageList.push('...');
           }
           if (this.pages > 1) this.pageList.push(this.pages.toString());
-
-
-
-
         }, error => {
           // this.dataService.data = error;
           // this.router.navigate(['/error']);
@@ -123,19 +131,12 @@ export class SearchComponent implements OnInit {
 
   setPage(page) {
     this.router.navigate(['/search'], {
-      queryParams: {'page': page},
+      queryParams: { 'page': page },
       queryParamsHandling: 'merge'
     });
   }
 
-  trackById(index: number, product: any){
+  trackById(index: number, product: any) {
     return product.id;
-  }
-
-  onSortChange(){
-    this.router.navigate(['/search'], {
-      queryParams: {'sort': this.selectedSortOption.value},
-      queryParamsHandling: 'merge'
-    });
   }
 }
