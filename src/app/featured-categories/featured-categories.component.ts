bac66d8d1a8a6bc877843c811b99e0bf485bd229
glapoint22 <glapoint22@gmail.com>
2018-01-18 13:31:23 -0500

    Completed featured categories

--- src/app/featured-categories/featured-categories.component.ts
+++ src/app/featured-categories/featured-categories.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataService } from "../data.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'featured-categories',
   templateUrl: './featured-categories.component.html',
   styleUrls: ['./featured-categories.component.scss']
 })
-export class FeaturedCategoriesComponent implements OnInit {
+export class FeaturedCategoriesComponent {
   private showFeatured: boolean = true;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private router: Router) { }
 
-  ngOnInit() {
+  onClick(category: number) {
+    this.router.navigate(['/search'], { queryParams: { 'category': category } });
   }
-
 }
